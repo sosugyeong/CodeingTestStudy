1dba083cbf0f56bc6f6342a7c7aa4e4eef56bc0d
Sugyeong <99548138+sosugyeong@users.noreply.github.com>
2025-12-05 22:56:11 +0900

    Add files via upload

---
+++ JavaStudy/Chapter07/Excercise7_10.java
@@ -0,0 +1,54 @@
+class MyTv2{
+    private boolean isPowerOn;
+    private int channel;
+    private int volume;
+    private int prevChannel;
+
+    final int MAX_VOLUME = 100;
+    final int MIN_VOLUME = 0;
+    final int MAX_CHANNEL = 100;
+    final int MIN_CHANNEL = 1;
+
+    public int getChannel() {
+        return channel;
+    }
+    public void setChannel(int channel) {
+        if (channel>MAX_CHANNEL || channel<MIN_CHANNEL) {
+            return;
+        }
+
+        prevChannel = this.channel;
+        this.channel = channel;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+    public void setVolume(int volume) {
+        if (volume>MAX_VOLUME || volume<MIN_VOLUME) {
+            return;
+        }
+        this.volume = volume;
+    }
+
+    void gotoPrevChannel(){
+        setChannel(prevChannel);
+    }
+}
+public class Excercise7_10 {
+    public static void main(String[] args) {
+        MyTv2 t = new MyTv2();
+
+        t.setChannel(10);
+        System.out.println("현재 채널: "+t.getChannel());
+        t.setVolume(20);
+        System.out.println("볼륨: "+t.getVolume());
+        t.setChannel(20);
+        System.out.println("현재 채널: "+t.getChannel());
+
+        t.gotoPrevChannel();
+        System.out.println("이전 채널: "+t.getChannel());
+        t.gotoPrevChannel();
+        System.out.println("이전 채널: "+t.getChannel());
+    }
+}
